b5c937ac61c972d329e7c1bc934620ea2b3a5a67
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2022-10-29 03:40:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import RepositoryList from "./components/RepositoryList";
+import './App.css';
+import RepositoryList from './components/RepositoryList';
 
 function App() {
-    const [count, setCount] = useState(0)
-
-    return (
-        <div className="App">
-            <RepositoryList/>
-        </div>
-    )
+  return (
+    <div className='App'>
+      <RepositoryList />
+    </div>
+  );
 }
 
-export default App
+export default App;
